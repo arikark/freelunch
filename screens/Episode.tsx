5ecc9df49c3c59b6dd332d2dfaa9c3e63af1fb17
Erankark <erank@ynomia.io>
2022-11-14 12:07:09 +1100

    added the description of the episode with see more button

--- screens/Episode.tsx
+++ screens/Episode.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import moment from 'moment'
 import {
   ArrowBackIcon,
@@ -8,6 +8,8 @@ import {
   HStack,
   IconButton,
   Image,
+  Pressable,
+  Spacer,
   Text,
   VStack,
 } from 'native-base'
@@ -26,7 +28,7 @@ type EpisodeProps = {
 const episode: EpisodeProps = {
   podcastTitle: 'The Daily',
   episodeTitle: 'How Democrats Can Win',
-  description: 'How the Democrats can win the 2020 election',
+  description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. `,
   dateCreated: '2022-11-09 00:00:00',
   durationInSeconds: 7200,
   secondsPlayed: 6023,
@@ -38,12 +40,18 @@ export default function Episode({
   route,
 }: PodcastStackScreenProps<'Episode'>) {
   const { title } = route.params
-
+  const [isFullDescription, setIsFullDescription] = useState(false)
   const formattedDate = moment(episode.durationInSeconds).fromNow()
   // Format time as 1 hour and 20 minutes left
   const timeRemaining = moment
     .duration(episode.durationInSeconds - episode.secondsPlayed, 'seconds')
     .humanize()
+
+  const isLongDescription = episode.description.length > 200
+  const description =
+    isLongDescription && !isFullDescription
+      ? `${episode.description.slice(0, 200)}...`
+      : episode.description
 
   return (
     <Box h="100%" paddingX={4} safeAreaTop safeAreaX variant="layout">
@@ -77,6 +85,18 @@ export default function Episode({
           <Button colorScheme="blue" rounded="2xl" width="88px">
             Play
           </Button>
+        </Box>
+        <Box>
+          <Text>
+            {description}{' '}
+            {isFullDescription || !isLongDescription ? null : (
+              <Pressable onPress={() => setIsFullDescription(true)}>
+                <Text bold color="white">
+                  see more
+                </Text>
+              </Pressable>
+            )}
+          </Text>
         </Box>
       </VStack>
     </Box>
